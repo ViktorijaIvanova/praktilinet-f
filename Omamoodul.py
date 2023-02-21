4c6c3a778c6eba31120567f2678abe303dc2a40e
opilane <opilane@HP-CZC2349HT1>
2023-02-21 14:05:04 +0200

    Add project files.

---
+++ Omamoodul.py
@@ -0,0 +1,46 @@
+from math import*
+def keskpalga(kp:int):
+    """
+    """
+    if kp>1200 or kp<1200:
+        inimesed=inimesed.index(inimesed.list)
+        print(inimesed.list)
+    if kp>1200 or kp<1200:
+        palgad=palgad.index(palgad.list)
+        print(palgad.list)
+        inimesed.append(inimesed)  
+        palgad.append(palgad)
+
+
+def keskpalga(inimesed:str,palgad:int):
+    """
+    """
+    if palgad>1200 or palgad<1200:
+        palgad.append(palgad)
+        print(palgad.append)
+    else:
+       inimesed.append(inimesed)  
+       print(inimesed.append)
+
+def keskpalga(inimesed:str,palgad:int):
+    """
+    """
+    if palgad>1200 or palgad<1200:
+       palgad.append
+    for i in range(len(palgad)):
+     print(palgad[i])
+    
+   
+
+def keskpalga(inimesed:int,palgad:int):
+    """
+    """
+    if palgad>1200 or palgad<1200:
+        palgad.join
+    for i in range(len(palgad)):
+     print(palgad[i]) 
+     if i in range (len(inimesed)):
+        print(inimesed[i])
+
+        
+        
---
+++ praktilinetööf.py
@@ -0,0 +1,7 @@
+from math import*
+from Omamoodul import*
+palgad=[1200,2500,750,395,1200]
+inimesed=["A","B","C","D","E"]         #keskmine palgad
+kp=keskpalga(int(input("kes palga suurem või väike?")))
+print(keskpalga)
+
